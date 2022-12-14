f63c2a1b89a280e65afc954c3cf298715facce20
i3oma <91991091+i3oma@users.noreply.github.com>
2022-12-14 16:07:35 +0100

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,6 @@
+function Set_Url(){
+    document.querySelector('#Facebook_share').href = `https://twitter.com/share?url=${window.location.href}`
+    document.querySelector('#Twitter_share').href = `https://www.facebook.com/sharer/sharer.php?u=${window.location.href}`
+    document.querySelector('#Whatsapp_share').href = `https://wa.me/?text=${window.location.href}`
+    document.querySelector('#telegram_share').href = `https://t.me/share/url?url=${window.location.href}`
+}
